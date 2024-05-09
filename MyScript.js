59b1d6eff89f3c3a1ce355b516225419f94dc060
Syeed Mohd Saquib <101272076+syeedsaquib@users.noreply.github.com>
2024-05-09 08:25:49 +0530

    initial commit

---
+++ MyScript.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector(".wrapper"),
+    qrInput = wrapper.querySelector(".form input"),
+    generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value;
+    if(!qrValue) return;
+    generateBtn.innerText = "Generating QR code...";
+    qrImg.src='https://api.qrserver.com/v1/create-qr-code/?size=170x170&data='+qrValue;
+    qrImg.addEventListener("load",()=> {
+        
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR code"
+
+
+    });
+});    
+
+qrInput.addEventListener("keyup",()=>{
+
+    if(!qrInput.value){
+        wrapper.classList.remove("active");
+    }
+
+});
